66f587e5792eeb0571e46c58a76bf0026ceeb875
Danny Suarez <dsuarezlogans@gmail.com>
2019-10-04 14:21:15 -0500

    add semantic-ui

--- src/App.js
+++ src/App.js
@@ -2,22 +2,18 @@ import React from 'react';
 import logo from './assets/Icon_512x512@3x.png';
 import './App.css';
 
+// import components here
+import Button from './components/TestButton';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Welcome to the World <code>Belaxito</code>.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          work in progress
-        </a>
+        <Button />
       </header>
     </div>
   );
