ca2148ef4804268d1c3103fb9bab00f9b0986648
Danny Suarez <dsuarezlogans@gmail.com>
2019-10-04 15:08:59 -0500

    add axios

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './index.css';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
